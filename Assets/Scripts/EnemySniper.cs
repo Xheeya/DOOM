13ef3ab0026c19098d7755228cbe30bc244f25d1
ricveldre <ricardo.velazquez@uteca.edu.mx>
2026-07-21 20:33:47 -0600

    EnemySniper ya funciona, códigos, SecurityGate
    Códigos de EnemySniper arreglado, el de SecurityGate y arreglamos el laser beam

--- Assets/Scripts/EnemySniper.cs
+++ Assets/Scripts/EnemySniper.cs
@@ -14,13 +14,14 @@ private float aimTime = 4f;
 private Text timerText;
 [SerializeField]
 private LaserBeam laserBeam;
-[SerializeField]
 private float nextFireTime;
 private bool IsInRange => Vector3.Distance(transform.position, player.position) <= range;
+private bool isShooting = false;
 public override void OnEnable()
     {
+        isShooting = false;
         base.OnEnable();
-        laserBeam.SetActive(false);
+        laserBeam.ActivateLaser(false);
         nextFireTime = 0f;
         transform.position = new Vector3(transform.position.x, 0f, transform.position.z);
         animator.Play("Idle", 0, 0f);
@@ -28,14 +29,19 @@ public override void OnEnable()
     }
     private void Update()
     {
-        if (IsInRange && Time.time >= nextFireTime)
+        if (health.IsDead) return;
+        if (CheckWin()) return;
+        transform.LookAt(player);
+        if (!isShooting && IsInRange && Time.time >= nextFireTime)
         {
+            isShooting = true;
             StartCoroutine(AimAndShoot());
-            nextFireTime = Time.time + fireRate;
         }
     }
     private IEnumerator AimAndShoot()
     {
+        laserBeam.Target = player;
+        laserBeam.ActivateLaser(true);
         SoundManager.instance.Play("sniper_spotted");
         animator.Play("Aim", 0, 0f);
         yield return animator.WaitForCurrentAnimation();
@@ -43,21 +49,23 @@ public override void OnEnable()
     }
     private IEnumerator Shoot ()
     {
-        laserBeam.SetActive(true);
-        laserBeam.Target = player;
         float duration = aimTime;
-        while (duration > 0f);
+        while (duration > 0f)
         {
-            duration --;
+            duration--;
             timerText.text = duration.ToString();
             yield return new WaitForSeconds(1f);
         }
+        animator.Play("Fire", 0, 0f);
         SoundManager.instance.Play("sniper_shoot");
-        laserBeam.SetActive(false);
+        laserBeam.ActivateLaser(false);
         player.GetComponent<Health>().TakeDamage(damage);
+        isShooting = false;
+        nextFireTime = Time.time + fireRate;
     }
     public override void Die()
     {
+        laserBeam.ActivateLaser(false);
         base.Die();
         SoundManager.instance.Play("sniper_death");
     }
